721df6fb25834343cb596243d20d8cc7e1c40a59
YoussefAlaaeldin <yousufalaa10@yahoo.com>
2024-11-02 09:14:15 +0300

    Initial Commit

---
+++ Netflix/NetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixApp.swift
+//  Netflix
+//
+//  Created by Yousuf Abdelfattah on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
